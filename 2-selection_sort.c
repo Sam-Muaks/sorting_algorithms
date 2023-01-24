4181d7bb98342eaaf0b93953c9f7a87d8aec6770
Emigham <emigham7@gmail.com>
2023-01-23 16:04:25 -0800

    coding is fun

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,44 +1,31 @@
 #include "sort.h"
 
 /**
- * counting_sort - sorts an array with the Counting sort algorithm
+ * selection_sort - sorts an array following the selection sort algorithm
  * @array: array to sort
- * @size: size of the array
+ * @size: size of the array;
  */
-void counting_sort(int *array, size_t size)
+void selection_sort(int *array, size_t size)
 {
-	int *count_arr, *out_arr, max, num, j, l;
-	size_t i, k, m, n;
+	unsigned int i, j, min;
+
+	register int tmp;
 
 	if (size < 2)
 		return;
 
-	max = array[0];
-	for (i = 1; i < size; i++)
-		if (array[i] > max)
-			max = array[i];
-
-	count_arr = malloc(sizeof(size_t) * (max + 1));
-	out_arr = malloc(sizeof(int) * size);
-
-	for (j = 0; j <= max; j++)
-		count_arr[j] = 0;
-	for (k = 0; k < size; k++)
+	for (i = 0; i < size; i++)
 	{
-		num = array[k];
-		count_arr[num] += 1;
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		tmp = array[i];
+		array[i] = array[min];
+		array[min] = tmp;
+		if (i != min)
+			print_array(array, size);
 	}
-	for (l = 1; l <= max; l++)
-		count_arr[l] += count_arr[l - 1];
-	print_array(count_arr, max + 1);
-	for (m = 0; m < size; m++)
-	{
-		out_arr[count_arr[array[m]] - 1] = array[m];
-		count_arr[array[m]]--;
-	}
-	for (n = 0; n < size; n++)
-		array[n] = out_arr[n];
-
-	free(count_arr);
-	free(out_arr);
 }
